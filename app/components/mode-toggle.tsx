d32dcdb86c7d040be7442f7c61746d6444a83ae8
mirrorgo <cloudmirrorgo@163.com>
2024-08-20 10:47:34 +1000

    feat: Update mode toggle button styles and icons

--- app/components/mode-toggle.tsx
+++ app/components/mode-toggle.tsx
@@ -18,7 +18,12 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className=" focus-visible:ring-0">
+        <Button
+          variant="ghost"
+          size="icon"
+          className="focus-visible:ring-0 focus:ring-0"
+          // className=" outline-none" 的写法不行，为什么
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
